d417b3cb5e0c279536e31bb3cd2edea789a342c5
JvJalandoni <s2120650@usls.edu.ph>
2025-04-30 15:47:00 +0800

    yes

--- src/com/ecpe308/sample/Pet.java
+++ src/com/ecpe308/sample/Pet.java
@@ -1,31 +1,42 @@
 package com.ecpe308.sample;
 
 public class Pet {
-    public String name, breed; //persian, labrador, golden retriever
-    public String pet_type;  //cat/dog
+    private String name;
+    private String breed; // persian, labrador, golden retriever
+    private String petType; // cat/dog
 
-    public String getName(){
+
+    public Pet(String name, String breed, String petType) {
+        this.name = name;
+        this.breed = breed;
+        this.petType = petType;
+    }
+
+
+    public String getName() {
         return name;
     }
 
-    public String getBreed(){
+    public String getBreed() {
         return breed;
     }
 
-    public String getPet_type(){
-        return pet_type;
+    public String getPetType() {
+        return petType;
     }
 
-    public void setName (String name){
+
+    public void setName(String name) {
         this.name = name;
     }
 
-    public void setBreed (String breed){
-        this.breed = breed; 
+    public void setBreed(String breed) {
+        this.breed = breed;
     }
-    
-    public void setPet_type (String pet_type){
-        this.pet_type= pet_type;
-    }
-}
 
+    public void setPetType(String petType) {
+        this.petType = petType;
+    }
+
+
+}
